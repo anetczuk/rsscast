6f1b16901e9708f1ab1d38e8e5d3d005d02afd63
anetczuk <anetczuk@o2.pl>
2023-07-09 22:00:06 +0200

    Add alternative converter

--- src/rsscast/rss/ytconverter.py
+++ src/rsscast/rss/ytconverter.py
@@ -31,6 +31,7 @@ from io import BytesIO
 import json
 import re
 
+import urllib
 from urllib import request
 from urllib.parse import urlencode
 
@@ -38,7 +39,9 @@ import ssl
 import pycurl
 
 from pytube import YouTube
+# import youtube_dl
 # import pafy
+
 import filetype
 
 
@@ -124,12 +127,36 @@ def get_yt_duration( link ):
     # return get_yt_duration_pafy( link )
 
 
+## ===================================================================
+
+
 def convert_yt( link, output, mimicHuman=True ):
-#     return convert_yt_dwnldr( link, output )
     succeed = convert_yt_yt1s( link, output, mimicHuman )
     if not succeed:
+        _LOGGER.error( f"failed to convert '{link}' - process failed" )
+        return False
+    if os.path.isfile( output ) is False:
+        _LOGGER.error( f"failed to convert '{link}' - missing file" )
         return False
     kind = filetype.guess( output )
+    if kind is None:
+        ## server respond with HTML page instead of audio file
+        _LOGGER.warning( f"failed to convert '{link}' - trying other method" )
+
+        succeed = convert_yt_y2down( link, output )
+        if not succeed:
+            _LOGGER.error( f"failed to convert '{link}' - unable to find type #1" )
+            os.remove( output )
+            return False
+        if os.path.isfile( output ) is False:
+            _LOGGER.error( f"failed to convert '{link}' - unable to find type #2" )
+            return False
+        kind = filetype.guess( output )
+        if kind is None:
+            _LOGGER.error( f"failed to convert '{link}' - unable to find type #3" )
+            os.remove( output )
+            return False
+
     if kind.extension != "mp3":
         _LOGGER.error( f"failed to convert '{link}' - bad type ({kind.extension})" )
         os.remove( output )
@@ -137,7 +164,148 @@ def convert_yt( link, output, mimicHuman=True ):
     return True
 
 
-## ===================================================================
+## converting YT videos using webpage https://yt1s.com
+def convert_yt_yt1s( link, output, mimicHuman=True ):
+    try:
+        session = get_curl_session()
+
+        mp3Data = get_mp3_data( session, link, mimicHuman )
+        if mp3Data is None:
+            return False
+
+        vidId     = mp3Data[0]
+        mp3Format = mp3Data[1]
+
+        convertId = mp3Format['k']
+#         dataSize  = mp3Format['size']
+
+#         pprint( mp3Format )
+
+        params = { 'vid': vidId,
+                   'k': convertId }
+        dataBuffer = curl_post( session, "https://yt1s.com/api/ajaxConvert/convert", params )
+        bodyOutput = dataBuffer.getvalue().decode('utf-8')
+#         _LOGGER.info( "response:\n%s", bodyOutput )
+
+        if mimicHuman:
+            randTime = random.uniform( 1.0, 3.0 )
+            time.sleep( randTime )
+
+        data = json.loads( bodyOutput )
+
+#         print( "convert response:", data )
+#         pprint( data )
+
+        jsonStatus = data['status']
+        if jsonStatus != "ok":
+            _LOGGER.warning( "invalid status:\n%s", bodyOutput )
+            return False
+        jsonCStatus = data['c_status']
+        if jsonCStatus != "CONVERTED":
+            #TODO: handle case
+            ## {"status":"ok","mess":"","c_status":"CONVERTING","b_id":"6004d4c0d684ebb22f8b45ab","e_time":39}
+            _LOGGER.warning( "invalid status:\n%s", bodyOutput )
+            return False
+
+        dlink = data["dlink"]
+
+        _LOGGER.info( "grabbing file: %s to %s", dlink, output )
+        urlretrieve( dlink, output )
+#         simple_download( dlink, output )
+#         curl_download( session, dlink, output )
+
+#         if mimicHuman:
+#             randTime = random.uniform( 1.0, 3.0 )
+#             time.sleep( randTime )
+
+        ## done -- returning
+        return True
+
+    except Exception:                                               # pylint: disable=W0703
+        _LOGGER.exception("Unexpected exception", exc_info=False)
+        return False
+
+    finally:
+        session.close()
+
+    return False
+
+
+## use https://y2down.cc/
+def convert_yt_y2down( link, output ):
+    ## https://loader.to/ajax/download.php?format=mp3&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1cpyexbmMyU
+    escaped_link = urllib.parse.quote( link )
+    convert_url = f"https://loader.to/ajax/download.php?format=mp3&url={escaped_link}"
+    convert_response = urlretrieve( convert_url )
+    convert_data = json.loads( convert_response )
+    # _LOGGER.info( f"convert data {convert_data}" )
+    if convert_data.get( "success", False ) is False:
+        _LOGGER.error( f"failed to convert '{link}' - server response" )
+        return False
+    convert_id = convert_data.get( "id", None )
+    if not convert_id:
+        _LOGGER.error( f"failed to convert '{link}' - missing ID" )
+        return False
+
+    # content = convert_data.get( "content", "" )
+    # content = base64.b64decode( content )
+    # content = content.decode("utf-8")
+    # print( f"content:\n{content}")
+
+    _LOGGER.info( f"waiting for finish of conversion of {link}" )
+
+    recent_progress_value    = 0
+    stalled_progress_counter = 0
+    while True:
+        progress_link = f"https://loader.to/ajax/progress.php?id={convert_id}"
+        progress_resp = urlretrieve( progress_link )
+        progress_data = json.loads( progress_resp )
+        _LOGGER.info( "received progress: %s %s", stalled_progress_counter, progress_data )
+        if progress_data.get("success", 0) != 0:
+            break
+        progress_value = progress_data.get("progress", 0)
+        if recent_progress_value == progress_value:
+            stalled_progress_counter += 1
+            if stalled_progress_counter >= 20:
+                _LOGGER.info( f"downloading stalled - breaking" )
+                return False
+        else:
+            recent_progress_value = progress_value
+            stalled_progress_counter = 0
+        time.sleep( 3.0 )
+
+    download_link = progress_data.get( "download_url" )
+    if not download_link:
+        return False
+
+    _LOGGER.info( f"downloading content from {download_link}" )
+    urlretrieve( download_link, output, write_empty=False )
+    return True
+
+
+# ## crashes with error:
+# ##    pytube.exceptions.RegexMatchError: get_throttling_function_name: could not find match for multiple
+# def convert_pytube( link, output, mimicHuman=True ):
+#     yt = YouTube( link )
+#     video = yt.streams.filter(only_audio=True).first()
+#     out_file = video.download( output_path="/tmp" )
+#     os.rename( out_file, output )
+#     return True
+
+
+# ### errors with 'uploader id'
+# def convert_youtube_dl( video_url, output ):
+#     video_info = youtube_dl.YoutubeDL().extract_info( url = video_url, download=False )
+#     filename = f"{video_info['title']}.mp3"
+#     options = { 'format': 'bestaudio/best',
+#                 'keepvideo': False,
+#                 'outtmpl': filename,
+#                 }
+#
+#     with youtube_dl.YoutubeDL(options) as ydl:
+#         ydl.download( [video_info['webpage_url']] )
+#
+#     print("Download complete... {}".format(filename))
 
 
 # def convert_yt_dwnldr( link, output, mimicHuman=True ):
@@ -259,73 +427,6 @@ def get_media_size( link, mimicHuman=True ):
     return None
 
 
-## converting YT videos using webpage https://yt1s.com
-def convert_yt_yt1s( link, output, mimicHuman=True ):
-    try:
-        session = get_curl_session()
-
-        mp3Data = get_mp3_data( session, link, mimicHuman )
-        if mp3Data is None:
-            return False
-
-        vidId     = mp3Data[0]
-        mp3Format = mp3Data[1]
-
-        convertId = mp3Format['k']
-#         dataSize  = mp3Format['size']
-
-#         pprint( mp3Format )
-
-        params = { 'vid': vidId,
-                   'k': convertId }
-        dataBuffer = curl_post( session, "https://yt1s.com/api/ajaxConvert/convert", params )
-        bodyOutput = dataBuffer.getvalue().decode('utf-8')
-#         _LOGGER.info( "response:\n%s", bodyOutput )
-
-        if mimicHuman:
-            randTime = random.uniform( 1.0, 3.0 )
-            time.sleep( randTime )
-
-        data = json.loads( bodyOutput )
-
-#         print( "convert response:" )
-#         pprint( data )
-
-        jsonStatus = data['status']
-        if jsonStatus != "ok":
-            _LOGGER.warning( "invalid status:\n%s", bodyOutput )
-            return False
-        jsonCStatus = data['c_status']
-        if jsonCStatus != "CONVERTED":
-            #TODO: handle case
-            ## {"status":"ok","mess":"","c_status":"CONVERTING","b_id":"6004d4c0d684ebb22f8b45ab","e_time":39}
-            _LOGGER.warning( "invalid status:\n%s", bodyOutput )
-            return False
-
-        dlink = data["dlink"]
-
-#         _LOGGER.info( "grabbing file: %s to %s", dlink, output )
-        urlretrieve( dlink, output )
-#         simple_download( dlink, output )
-#         curl_download( session, dlink, output )
-
-#         if mimicHuman:
-#             randTime = random.uniform( 1.0, 3.0 )
-#             time.sleep( randTime )
-
-        ## done -- returning
-        return True
-
-    except Exception:                                               # pylint: disable=W0703
-        _LOGGER.exception("Unexpected exception", exc_info=False)
-        return False
-
-    finally:
-        session.close()
-
-    return False
-
-
 ## perform 'POST' request on curl session
 def curl_post( session, targetUrl, dataDict ):
 #     _LOGGER.info( "accessing url: %s params: %s", targetUrl, dataDict )
@@ -372,7 +473,7 @@ def curl_download_raw( session, sourceUrl, outputFile ):
         os.remove( path )
 
 
-def urlretrieve( url, outputPath ):
+def urlretrieve( url, outputPath=None, timeout=30, write_empty=True ):
     ##
     ## Under Ubuntu 20 SSL configuration has changed causing problems with SSL keys.
     ## For more details see: https://forums.raspberrypi.com/viewtopic.php?t=255167
@@ -384,25 +485,27 @@ def urlretrieve( url, outputPath ):
 
     ## changed "user-agent" fixes blocking by server
     req = request.Request( url, headers={'User-Agent': 'Mozilla/5.0'} )
-    with request.urlopen( req, timeout=30, context=ctx_no_secure ) as result:
+    with request.urlopen( req, timeout=timeout, context=ctx_no_secure ) as result:
 
     #     result = request.urlopen( url, context=ctx_no_secure )
         content_data = result.read()
 
-        try:
-            with open(outputPath, 'wb') as of:
-                of.write( content_data )
+        if outputPath:
+            if len(content_data) > 0 or write_empty: 
+                try:
+                    with open(outputPath, 'wb') as of:
+                        of.write( content_data )
 
-    #         content_text = content_data.decode("utf-8")
-    #         with open(outputPath, 'wt') as of:
-    #             of.write( content_text )
+        #         content_text = content_data.decode("utf-8")
+        #         with open(outputPath, 'wt') as of:
+        #             of.write( content_text )
 
-        except UnicodeDecodeError as ex:
-            _LOGGER.exception( "unable to access: %s %s", url, ex, exc_info=False )
-            raise
-
-    #     urllib.request.urlretrieve( url, outputPath, context=ctx_no_secure )
-    #     urllib.request.urlretrieve( url, outputPath )
+                except UnicodeDecodeError as ex:
+                    _LOGGER.exception( "unable to access: %s %s", url, ex, exc_info=False )
+                    raise
+    
+        #     urllib.request.urlretrieve( url, outputPath, context=ctx_no_secure )
+        #     urllib.request.urlretrieve( url, outputPath )
 
         return content_data
 
--- src/testrsscast/integration/intgr_ytconverter.py
+++ src/testrsscast/integration/intgr_ytconverter.py
@@ -27,21 +27,51 @@ logging.basicConfig( level=logging.DEBUG )
 ## =================================================================
 
 
-url = 'https://www.youtube.com/watch?v=gNs_9xFisdE'
-duration = get_yt_duration( url )
-if duration != 1152:
-    _LOGGER.error( "===== test failed: unable to read video proper length" )
-else:
-    length_m = int(duration / 60)
-    length_s = int(duration % 60)
-    _LOGGER.info( f'length: {duration} = {length_m}m {length_s}s' )
+def test_01():
+    url = 'https://www.youtube.com/watch?v=gNs_9xFisdE'
+    duration = get_yt_duration( url )
+    if duration != 1152:
+        _LOGGER.error( "===== test failed: unable to read video proper length" )
+    else:
+        length_m = int(duration / 60)
+        length_s = int(duration % 60)
+        _LOGGER.info( f'length: {duration} = {length_m}m {length_s}s' )
+    
+    output_path = "/tmp/test_convert_00.data"
+    _LOGGER.info( "converting video: %s into %s", url, output_path )
+    
+    status = convert_yt( url, output_path )
+    _LOGGER.info( "conversion status: %s", status )
+    if not status:
+        _LOGGER.error( "===== test failed: unable to convert video" ) 
 
-output_path = "/tmp/test_convert.data"
-_LOGGER.info( "converting video: %s into %s", url, output_path )
 
-status = convert_yt( url, output_path )
-_LOGGER.info( "conversion status: %s", status )
-if not status:
-    _LOGGER.error( "===== test failed: unable to convert video" ) 
+def test_02():
+    url = "https://www.youtube.com/watch?v=1cpyexbmMyU"
+    output_path = "/tmp/test_convert_01.data"
+    _LOGGER.info( f"downloading {url}" )
+    status = convert_yt( url, output_path )
+    _LOGGER.info( "conversion status: %s", status )
+    if not status:
+        _LOGGER.error( "===== test failed: unable to convert video" )
+
+
+def test_03():
+    url = "https://www.youtube.com/watch?v=ZGMNaHINvuI"
+    #url = "https://www.youtube.com/watch?v=2p8OTosmFHU"
+    output_path = "/tmp/test_convert_02.data"
+    _LOGGER.info( f"downloading {url}" )
+    status = convert_yt( url, output_path )
+    _LOGGER.info( "conversion status: %s", status )
+    if not status:
+        _LOGGER.error( "===== test failed: unable to convert video" )
+
+
+# test_01()
+
+# test_02()
+
+test_03()
+
 
 _LOGGER.info( "all tests done" )
